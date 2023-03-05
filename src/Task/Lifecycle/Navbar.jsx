78712ae019fa1fce3868400a84eaff0492d28246
Ryanprayyy <79523005+mrxxx02@users.noreply.github.com>
2023-03-05 12:27:38 +0700

    Add files via upload

---
+++ src/Task/Lifecycle/Navbar.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-primary bg-primary">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            NEWS APP
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-0 mb-lg-1" >
+              <li className="nav-item">
+                <Link className="nav-link" to="/business">
+                  Bisnis
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/entertainment">
+                  Hiburan
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/health">
+                  Kesehatan
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/science">
+                  Pengetahuan
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/sports">
+                  Olahraga
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/technology">
+                  Teknologi
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
