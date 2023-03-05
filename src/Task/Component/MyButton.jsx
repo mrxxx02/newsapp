0e518cc18844fc8e74d483068f5d3af86b90bc33
Ryanprayyy <79523005+mrxxx02@users.noreply.github.com>
2023-03-05 12:26:17 +0700

    Delete src directory

--- src/Task/Component/MyButton.jsx
+++
@@ -1,9 +0,0 @@
-const MyButton = (props) => {
-    return (
-        <div>
-            <button onClick={() => props.clicked()}>Click Me!!</button>
-        </div>
-    );
-};
-
-export default MyButton;
--- src/Task/Routing/Home.jsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-    return <h1>WELCOME TO MY WEBSITE😎💕</h1>
-  }
